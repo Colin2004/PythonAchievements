e7067d92e6a31b9ff8d0a0f028e110228507396b
Colin de Groot <colindegroot2004@gmail.com>
2020-09-23 12:35:23 +0200

    De opdracht PYTB1L2FamliyTies toegevoegd

---
+++ PYTB1L2FamliyTies/PYTB1L2FamliyTies.py
@@ -0,0 +1,14 @@
+Naam = "Colin"
+Leeftijd = 15
+Woonplaats = "Purmerend"
+Afstand = 21,2
+Games = "Rainbow six siege en Forza Horizon 4"
+NogmeerGames = ""
+Hond = "Jackrussel"
+Sport = "waterpolo"
+
+print("Mijn naam is" ,Naam ,"ik ben", Leeftijd, "jaar oud en ik woon in", Woonplaats)
+print("De afstand tussen mij en school is ongeveer", Afstand, "kilometer van school.")
+print("De games die ik nu het meeste speel zijn",Games)
+print("Verder heb ik een",Hond)
+print("En doe ik aan", Sport)
