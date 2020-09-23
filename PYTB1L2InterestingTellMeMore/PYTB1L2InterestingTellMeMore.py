794b5a0853830b331f28beb558e40a9f28966812
Colin de Groot <colindegroot2004@gmail.com>
2020-09-23 12:12:15 +0200

    PYTB1L2InterestingTellMeMore toegevoegd

---
+++ PYTB1L2InterestingTellMeMore/PYTB1L2InterestingTellMeMore.py
@@ -0,0 +1,12 @@
+print("Hallo ik ben Colin.")
+print("Om te weten wie jij bent stel ik je drie vragen.")
+print("")
+
+
+Naam = input("Wat is jouw naam? ")
+Leeftijd = input("Hoevel jaar oud ben je? ")
+Woonplaats = input("Waar woon je? ")
+
+print("Oke jouw naam is dus",Naam)
+print("Je bent",Leeftijd)
+print("En je woont in",Woonplaats)
