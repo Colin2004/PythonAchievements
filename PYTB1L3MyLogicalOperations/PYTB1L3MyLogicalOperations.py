2e099fdca8df98abab226e1b398caec4d5c540ea
Colin de Groot <colindegroot2004@gmail.com>
2020-10-07 12:26:11 +0200

    PYTB1L3MyLogicalOperations toegevoegd

---
+++ PYTB1L3MyLogicalOperations/PYTB1L3MyLogicalOperations.py
@@ -0,0 +1,14 @@
+naam = "Colin"
+hobby = "gamen"
+aantalgames = 45
+
+def DeleteSomeGames():
+    for index in range(1):
+        print("Doe een game weg.")
+
+
+if naam == "Colin" and hobby != None or aantalgames > 45:
+    DeleteSomeGames()
+
+
+
