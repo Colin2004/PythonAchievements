cc57803c15023020551e4595d72ba47c8a5cdb74
Colin de Groot <colindegroot2004@gmail.com>
2020-10-21 12:24:48 +0200

    PYTB1L5Scooter verbeterd

--- PYTB1L5Scooter/PYTB1L5Scooter.py
+++ PYTB1L5Scooter/PYTB1L5Scooter.py
@@ -1,19 +1,17 @@
-invoer = input();
+invoer = input("Hoeveel km per maand rij je?: ");
 verzekering_per_maand = 23
 benzine_kosten_per_liter = 1.54
 liter_per_kilometer = 0.2
-km_per_maand = int(invoer)
+km_per_maand = float(invoer)
 
 
-maandkosten = km_per_maand * 0,2 * 1.54 + 23
+maandkosten = km_per_maand * 0.2 * 1.54 + 23
 
 
-while not isinstance(invoer, float):
+if isinstance(km_per_maand, float):
 
   
     try:
-        invoer = input("Voer een getal in: ")
-        invoer = float(invoer)
         print("De kosten zijn", maandkosten)
 
     except ValueError:
