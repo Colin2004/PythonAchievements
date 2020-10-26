57c8e917d43171746e3aaba566944e095eb094d5
Colin de Groot <colindegroot2004@gmail.com>
2020-10-26 21:10:37 +0100

    PYTB1L6CanYouHandleTheList toegevoegd

---
+++ PYTB1L6CanYouHandleTheList/PYTB1L6CanYouHandleTheList.py
@@ -0,0 +1,16 @@
+lijst = ["erg", 420, False] #list aangemaakt
+
+print(lijst)
+lijst.append("boterkoek") #hoi toegevoegd aan de list
+
+print(lijst)
+lijst.remove(420)
+lijst.remove(False) #verwijderen van de variabelen in een list
+
+print(lijst)
+lijst.append("lekker")
+lijst.sort()
+lijst.append("!")
+
+
+print(lijst)
